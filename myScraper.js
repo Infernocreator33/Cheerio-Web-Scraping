549cb6820bf97bce3e568608aa3aed43adb4048b
zsexton33 <zs0877310@otc.edu>
2019-05-28 09:23:00 -0500

    updated to use lodash for most frequent

--- myScraper.js
+++ myScraper.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const cheerio = require('cheerio');
-
+const _ = require('lodash');
 //Choose html page to scrape **features page has the content I want
 request('https://banno.com/features/', (error, response, html) =>
 {
@@ -16,24 +16,24 @@ request('https://banno.com/features/', (error, response, html) =>
         console.log("List of Products available: " + products.text().replace(/\s\s+/g, ''));
         //find the most common character in the html file and how often it is used
 
-        // const mostFrequent = Array.from(html).map(mostFrequent =>
-        //     {
-        //         mostFrequent.innerText.split('');
-        //     });
-        //     var charTemp = mostFrequent.toString();
-        //     var findMostFrequent = (string) => {
-        //         var max = 0;
-        //         var maxChar = '';
-        //         string.split('').forEach((char) => {
-        //             if(string.split(char).length > max) {
-        //                 max = string.split(char).length;
-        //                 maxChar = char;
-        //             }
-        //         });
-        //         console.log("Number of times max character is used: " + max);
-        //         return maxChar;
-        //     }
-        //     console.log("Most used character: " + findMostFrequent(charTemp));
+        const mostFrequent = Array.from(html).map(mostFrequent =>
+            {
+                _.chunk(mostFrequent.innerText, 1);
+            });
+            var charTemp = mostFrequent.toString();
+            var findMostFrequent = (string) => {
+                var max = 0;
+                var maxChar = '';
+                string.split('').forEach((char) => {
+                    if(string.split(char).length > max) {
+                        max = string.split(char).length;
+                        maxChar = char;
+                    }
+                });
+                console.log("Number of times max character is used: " + max);
+                return maxChar;
+            }
+            console.log("Most used character: " + findMostFrequent(charTemp));
         //find the number of .png files in the html file
         const numImages = Array.from($('body img')).map(numImages => numImages.src);
         console.log("Images total: " + numImages.length);
